eebccc539b586ab4d4429e50243fcd9ddaa146d3
benji dalton <daylightsavings@gmail.com>
2018-12-03 06:18:46 -0500

    handle no user

--- controllers/sessions.mjs
+++ controllers/sessions.mjs
@@ -4,13 +4,15 @@ import Auth from '../lib/auth'
 export default {
 
   async create (req, resp) {
-    console.log(req.body)
     const user = await User.findOne({email: req.body.email})
+    if (!user) {
+      return resp.status(401).send({msg: 'user not found'})
+    }
     try {
       const session = await Auth.createToken(user, req.body.password)
       resp.json(session)
     } catch (error) {
-      resp.send(error)
+      resp.status(401).send({msg: error})
     }
   }
 
