f34d40157b9f6043542ad3c1037809c9eb50fc5e
benji dalton <daylightsavings@gmail.com>
2019-01-03 00:16:29 -0500

    handle login failure better

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -5,14 +5,13 @@ export default {
 
   async create (req, resp) {
     const user = await User.findOne({email: req.body.email})
-    if (!user) {
-      return resp.status(401).send({msg: 'user not found'})
-    }
+    if (!user) { return resp.status(401).send({msg: 'user not found'}) }
     try {
       const session = await Auth.createToken(user, req.body.password)
+      if (!session) { return resp.status(401).send({msg: 'incorrect login'}) }
       resp.json(session)
     } catch (error) {
-      resp.status(401).send({msg: error})
+      resp.status(500).send({msg: error})
     }
   }
 
