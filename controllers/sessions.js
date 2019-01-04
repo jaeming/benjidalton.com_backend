93e5b69f95b65e07dedf2d3c4672826874bf7aab
benji dalton <daylightsavings@gmail.com>
2019-01-04 05:03:57 -0500

    auth adjustment

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -8,7 +8,7 @@ export default {
     if (!user) { return resp.status(401).send({msg: 'user not found'}) }
     try {
       const session = await Auth.createToken(user, req.body.password)
-      if (!session) { return resp.status(401).send({msg: 'incorrect login'}) }
+      if (!session.authenticated) { return resp.status(401).send({msg: 'incorrect login'}) }
       resp.json(session)
     } catch (error) {
       resp.status(401).send({msg: error})
