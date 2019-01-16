b133cdc90327365d4a16eaf98c89c76b510cfa10
benji dalton <daylightsavings@gmail.com>
2019-01-16 06:06:40 -0500

    restrict image access

--- config/host.js
+++ config/host.js
@@ -1,4 +1,7 @@
 const prod = 'https://api.benjidalton.com'
 const dev = 'http://localhost:3000'
 const url = process.env.NODE_ENV === 'production' ? prod : dev
-export default { url }
+
+const clients = ['benjidalton.com', 'api.benjidalton.com', 'localhost']
+
+export default { url, clients }
--- controllers/images.js
+++ controllers/images.js
@@ -3,19 +3,34 @@ import fs from 'fs'
 import path from 'path'
 import Image from '../models/image'
 
+const authorizedClient = (req, resp) => {
+  console.log(req.hostname)
+  const isAllowed = host.clients.includes(req.hostname)
+  if (isAllowed) {
+    return true
+  } else {
+    resp.status(401).json({error: 'Not Authorized'})
+    return false
+  }
+}
+
 export default {
   create (req, resp) {
-    let image = new Image({
-      name: req.file.filename,
-      contentType: req.body,
-      meta: req.file
-    })
-    image.save()
-    resp.json({url: `${host.url}/images/${image.name}`})
+    if (authorizedClient(req, resp)) {
+      let image = new Image({
+        name: req.file.filename,
+        contentType: req.body,
+        meta: req.file
+      })
+      image.save()
+      resp.json({url: `${host.url}/images/${image.name}`})
+    }
   },
 
   async show (req, resp) {
-    let image = await Image.findOne({name: req.params.name})
-    fs.createReadStream(path.join('uploads/images/', image.name)).pipe(resp)
+    if (authorizedClient(req, resp)) {
+      let image = await Image.findOne({name: req.params.name})
+      fs.createReadStream(path.join('uploads/images/', image.name)).pipe(resp)
+    }
   }
 }
