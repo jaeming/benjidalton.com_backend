f78bf8af54ecbd1f883ec0150e9893e4eef8b2fb
benji dalton <daylightsavings@gmail.com>
2019-01-16 08:04:39 -0500

    remove api from image restriction

--- config/host.js
+++ config/host.js
@@ -1,8 +1,9 @@
-const frontend = 'https://benjidalton.com'
+const frontendProd = 'https://benjidalton.com'
+const frontendDev = 'http://localhost:8080'
 const prod = 'https://api.benjidalton.com'
 const dev = 'http://localhost:3000'
 const url = process.env.NODE_ENV === 'production' ? prod : dev
 
-const clients = [frontend, prod, dev]
+const clients = [frontendProd, frontendDev]
 
 export default { url, clients }
