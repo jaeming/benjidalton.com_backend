9bc4d0fae61a8134b719e44a94828c2794187ee2
benji dalton <daylightsavings@gmail.com>
2019-01-16 22:40:38 -0500

    don't need this for now

--- config/host.js
+++ config/host.js
@@ -1,9 +1,5 @@
-const frontendProd = 'https://benjidalton.com'
-const frontendDev = 'http://localhost:8080'
 const prod = 'https://api.benjidalton.com'
 const dev = 'http://localhost:3000'
 const url = process.env.NODE_ENV === 'production' ? prod : dev
 
-const clients = [frontendProd, frontendDev]
-
-export default { url, clients }
+export default { url }
