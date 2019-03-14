95bf971b2fe54d29873e20399315c5060dce7878
benji dalton <daylightsavings@gmail.com>
2019-03-14 00:38:54 -0400

    add return response to terminate, remove auto-gen flickr files

--- controllers/sessions.js
+++ controllers/sessions.js
@@ -6,7 +6,7 @@ export default {
 
   async create (req, resp) {
     const user = await User.findOne({email: req.body.email})
-    if (!user) { response.unauthorized(resp) }
+    if (!user) { return response.unauthorized(resp) }
     const session = await Auth.createToken(user, req.body.password)
     if (!session.authenticated) { return response.unauthorized(resp) }
     resp.json(session.token)
