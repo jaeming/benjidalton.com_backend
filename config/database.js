c6caabff5f10cec72a2cf819ca921231d6cf1830
Benji <daylightsavings@gmail.com>
2020-06-03 23:25:04 -0400

    update deps and fix mongoose deprecation warning

--- config/database.js
+++ config/database.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose'
 export default {
   init () {
     mongoose.connect(process.env.DB_HOST)
+    mongoose.set('useCreateIndex', true)
     mongoose.Promise = global.Promise
     let db = mongoose.connection
     db.on('error', console.error.bind(console, 'MongoDB connection error:'))
